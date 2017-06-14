f31a8d6ea16032929c7a6a8afc8bec431f2aa5e8
Chuck <gvip888@gmail.com>
2017-06-14 17:06:24 +0800

    API for Minecraft Pi init
    Signed-off-by: Chuck <gvip888@gmail.com>

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mcpi',
+		version='1.9',
+		description='API for Minecraft Pi',
+		author='zhuowei & chuck mod',
+		url='https://www.chuckl.vip',
+		packages=['mcpi']
+)
